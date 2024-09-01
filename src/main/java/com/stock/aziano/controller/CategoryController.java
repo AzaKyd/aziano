e63d9f2b68ddb69f4a56c60b7b4655da684ec34f
AzaKyd <azamat.kydyraliev@gmail.com>
2024-09-01 09:29:25 +0600

    01 09 24

--- src/main/java/com/stock/aziano/controller/CategoryController.java
+++ src/main/java/com/stock/aziano/controller/CategoryController.java
@@ -6,23 +6,31 @@ import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/category")
 public class CategoryController {
 
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
 
     @Autowired
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
-    @GetMapping("/categories")
+    @GetMapping("")
     public String categories(Model model) {
         List<CategoryDto> categories = categoryService.getCategories();
         model.addAttribute("categories", categories);
         return "categories";
+    }
+
+    @GetMapping("/add")
+    public String addCategory(Model model) {
+
+        return "/forms/category/add-category";
     }
 }
