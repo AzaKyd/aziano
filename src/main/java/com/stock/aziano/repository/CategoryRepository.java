379a742d72160c3003ca3f6535036f0fd86a247e
Азамат <azamat.kydyraliev@gmail.com>
2024-11-08 02:31:28 +0600

    profile add

--- src/main/java/com/stock/aziano/repository/CategoryRepository.java
+++ src/main/java/com/stock/aziano/repository/CategoryRepository.java
@@ -2,7 +2,9 @@ package com.stock.aziano.repository;
 
 import com.stock.aziano.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByName(String name);
 }
